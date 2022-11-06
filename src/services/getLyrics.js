7b73194c634470f5e9a24e01925a8499122caf4e
arsen1c <vector2912@gmail.com>
2022-11-06 17:19:34 +0530

    Removed gaana command

--- src/services/getLyrics.js
+++ src/services/getLyrics.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { fetchHTML, iterateHTML } from '../helpers';
 
 const lyreka = async (songName) => {
@@ -44,50 +43,6 @@ const lyreka = async (songName) => {
     });
 };
 
-// [+] From Gaana.com [+]
-const getSuggestions = async (lyrics) => {
-  const URL = `https://gsearch.gaana.com/gaanasearch-api/mobilesuggest/autosuggest-lite-vltr-ro?geoLocation=IN&query=${lyrics}&content_filter=2&include=allItems&isRegSrch=0&webVersion=mix&rType=web&usrLang=Hindi,English,Punjabi`;
-  let suggestions = `Didn't find the song, but I have some suggestions for ya.\nType: /gaana (song_suggestion)\n\n`;
-
-  try {
-    const { data } = await axios.get(URL);
-
-    // console.log(JSON.stringify(data, null, 2))
-    data.gr[0].gd.forEach((song) => {
-      suggestions += `<b>${song.ti} (${song.sti})</b>\n/gaana ${song.seo
-        .split('-')
-        .join(' ')}\n\n`;
-    });
-
-    return suggestions;
-  } catch (error) {
-    console.log(error);
-    return `Error: ${error.message}`;
-  }
-};
-
-const gaana = async (song) => {
-  const URL = `https://gaana.com/song/${song.join('-')}`;
-  const data = fetchHTML(URL);
-
-  return data
-    .then(async (html) => {
-      let body = ``;
-      let lyrics = html('section.lyrics > div.data > p').text();
-      if (!lyrics) throw new Error("Lyrics not found")
-      let album = html('._b > _name > a').text();
-      let year = html('._b > _date').text();
-      let artists = html('ul.singers').text();
-
-      body += `<b>Album:</b> ${album} - ${year}\n<b>Artist[s]</b>: ${artists}\n\n<b>Lyrics:</b>\n\n${lyrics}`;
-      return { markdown: body };
-    })
-    .catch(async (error) => {
-      return { markdown: await getSuggestions(song.join('%20')) };
-    });
-};
-
 export default {
-  lyreka,
-  gaana,
+  lyreka
 };
